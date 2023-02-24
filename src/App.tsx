b78dce544144b27c62e8d781ec8c4431ac335521
Jess Delgado Perez <jess@include.io>
2023-02-24 08:52:07 -0600

    feat: add svgs and render as a child

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,51 @@
 import React from 'react';
 import './styles/app.css';
 
+interface SocialLinkInfo {
+  url: string;
+  image: string;
+  name: string;
+}
+
 function App() {
+  const renderSocialLink = ({ url, image, name }: SocialLinkInfo) => {
+    return (
+      <li className="p-6">
+        <a
+          className="flex flex-row"
+          href={url}
+          target="_blank"
+          rel="noreferrer"
+        >
+          <img
+            className="max-w-[2rem] mr-2"
+            src={`/social/${image}.svg`}
+            alt={name}
+          />
+          {name}
+        </a>
+      </li>
+    );
+  };
+
+  const socialLinks: SocialLinkInfo[] = [
+    {
+      url: 'https://github.com/jesatrix',
+      image: 'github',
+      name: 'GitHub'
+    },
+    {
+      url: 'https://www.linkedin.com/in/jessicadelgado/',
+      image: 'linkedin',
+      name: 'LinkedIn'
+    },
+    {
+      url: 'https://twitter.com/jesatrix',
+      image: 'twitter',
+      name: 'Twitter'
+    }
+  ];
+
   return (
     <div className="">
       <header className="app-header">
@@ -9,35 +53,9 @@ function App() {
           <span>🧑🏽‍💻</span> Jess Delgado Perez
         </h1>
       </header>
-      <main>
-        <ul>
-          <li>
-            <a
-              href="https://github.com/jesatrix"
-              target="_blank"
-              rel="noreferrer"
-            >
-              GitHub
-            </a>
-          </li>
-          <li>
-            <a
-              href="https://www.linkedin.com/in/jessicadelgado/"
-              target="_blank"
-              rel="noreferrer"
-            >
-              LinkedIn
-            </a>
-          </li>
-          <li>
-            <a
-              href="https://twitter.com/jesatrix"
-              target="_blank"
-              rel="noreferrer"
-            >
-              Twitter
-            </a>
-          </li>
+      <main className="flex flex-col justify-center items-center">
+        <ul className="flex flex-col max-w-md justify-around text-2xl">
+          {socialLinks.map((link) => renderSocialLink(link))}
         </ul>
       </main>
     </div>
